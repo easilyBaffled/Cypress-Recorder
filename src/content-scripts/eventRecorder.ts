9aecae8cfd688a5878bc8e2cdfad6272249bd922
Dan Michaelis <dmichaelis0+github@gmail.com>
2020-12-30 18:52:47 -0500

    add `contains` as selector

--- src/content-scripts/eventRecorder.ts
+++ src/content-scripts/eventRecorder.ts
@@ -3,9 +3,9 @@
  *
  * Responsible for recording the DOM events.
  */
-import finder from '@medv/finder';
-import type { ParsedEvent } from '../types';
-import { EventType } from '../constants';
+import finder from "@medv/finder";
+import type { ParsedEvent } from "../types";
+import { EventType } from "../constants";
 
 let port: chrome.runtime.Port;
 
@@ -15,24 +15,50 @@ let port: chrome.runtime.Port;
  * @returns {ParsedEvent}
  */
 function parseEvent(event: Event): ParsedEvent {
-  let selector: string;
-  if ((event.target as Element).hasAttribute('data-cy')) selector = `[data-cy=${(event.target as Element).getAttribute('data-cy')}]`;
-  else if ((event.target as Element).hasAttribute('data-test')) selector = `[data-test=${(event.target as Element).getAttribute('data-test')}]`;
-  else if ((event.target as Element).hasAttribute('data-testid')) selector = `[data-testid=${(event.target as Element).getAttribute('data-testid')}]`;
-  else selector = finder(event.target as Element, { attr: (name, value) => {
-    return name === 'data-cy' || name === 'data-test' || name === 'data-testid';
-  }});
-  const parsedEvent: ParsedEvent = {
-    selector,
-    action: event.type,
-    tag: (event.target as Element).tagName,
-    value: (event.target as HTMLInputElement).value,
-  };
-  if ((event.target as HTMLAnchorElement).hasAttribute('href')) parsedEvent.href = (event.target as HTMLAnchorElement).href;
-  if ((event.target as Element).hasAttribute('id')) parsedEvent.id = (event.target as Element).id;
-  if (parsedEvent.tag === 'INPUT') parsedEvent.inputType = (event.target as HTMLInputElement).type;
-  if (event.type === 'keydown') parsedEvent.key = (event as KeyboardEvent).key;
-  return parsedEvent;
+    let selector: string;
+    if ((event.target as Element).hasAttribute("data-cy"))
+        selector = `[data-cy=${(event.target as Element).getAttribute(
+            "data-cy"
+        )}]`;
+    else if ((event.target as Element).hasAttribute("data-test"))
+        selector = `[data-test=${(event.target as Element).getAttribute(
+            "data-test"
+        )}]`;
+    else if ((event.target as Element).hasAttribute("data-testid"))
+        selector = `[data-testid=${(event.target as Element).getAttribute(
+            "data-testid"
+        )}]`;
+    else
+        selector =
+            (event.target as Element,
+            {
+                attr: (name, value) => {
+                    return (
+                        name === "data-cy" ||
+                        name === "data-test" ||
+                        name === "data-testid"
+                    );
+                }
+            });
+    // @ts-ignore
+    const innerText = event.target.innerText || null;
+    const parsedEvent: ParsedEvent = {
+        selector,
+        // @ts-ignore
+        innerText,
+        action: event.type,
+        tag: (event.target as Element).tagName,
+        value: (event.target as HTMLInputElement).value
+    };
+    if ((event.target as HTMLAnchorElement).hasAttribute("href"))
+        parsedEvent.href = (event.target as HTMLAnchorElement).href;
+    if ((event.target as Element).hasAttribute("id"))
+        parsedEvent.id = (event.target as Element).id;
+    if (parsedEvent.tag === "INPUT")
+        parsedEvent.inputType = (event.target as HTMLInputElement).type;
+    if (event.type === "keydown")
+        parsedEvent.key = (event as KeyboardEvent).key;
+    return parsedEvent;
 }
 
 /**
@@ -40,37 +66,37 @@ function parseEvent(event: Event): ParsedEvent {
  * @param event
  */
 function handleEvent(event: Event): void {
-  if (event.isTrusted === true) port.postMessage(parseEvent(event));
+    if (event.isTrusted === true) port.postMessage(parseEvent(event));
 }
 
 /**
  * Adds event listeners to the DOM.
  */
 function addDOMListeners(): void {
-  Object.values(EventType).forEach(event => {
-    document.addEventListener(event, handleEvent, {
-      capture: true,
-      passive: true,
+    Object.values(EventType).forEach((event) => {
+        document.addEventListener(event, handleEvent, {
+            capture: true,
+            passive: true
+        });
     });
-  });
 }
 
 /**
  * Removes event listeners from the DOM.
  */
 function removeDOMListeners(): void {
-  Object.values(EventType).forEach(event => {
-    document.removeEventListener(event, handleEvent, { capture: true });
-  });
+    Object.values(EventType).forEach((event) => {
+        document.removeEventListener(event, handleEvent, { capture: true });
+    });
 }
 
 /**
  * Initializes the event recorder.
  */
 function initialize(): void {
-  port = chrome.runtime.connect({ name: window.location.hostname });
-  port.onDisconnect.addListener(removeDOMListeners);
-  addDOMListeners();
+    port = chrome.runtime.connect({ name: window.location.hostname });
+    port.onDisconnect.addListener(removeDOMListeners);
+    addDOMListeners();
 }
 
 initialize();
--- src/helpers/codeGenerator.ts
+++ src/helpers/codeGenerator.ts
@@ -4,8 +4,13 @@
  * Each time the user records, this function will generate a cy.visit command that will
  * store the current url, as well each subsequest user interaction with the browser.
  */
-import type { ParsedEvent } from '../types';
-import { EventType } from '../constants';
+import type { ParsedEvent } from "../types";
+import { EventType } from "../constants";
+
+const selection = (event: ParsedEvent) =>
+    event.innerText
+        ? `contains('${event.innerText}')`
+        : `get('${event.selector}')`;
 
 /**
  * Helper functions that handle each action type.
@@ -13,63 +18,63 @@ import { EventType } from '../constants';
  */
 
 function handleClick(event: ParsedEvent): string {
-  return `cy.get('${event.selector}').click();`;
+    return `cy.${selection(event)}.click();`;
 }
 
 function handleKeydown(event: ParsedEvent): string | null {
-  switch (event.key) {
-    case 'Backspace':
-      return `cy.get('${event.selector}').type('{backspace}');`;
-    case 'Escape':
-      return `cy.get('${event.selector}').type('{esc}');`;
-    case 'ArrowUp':
-      return `cy.get('${event.selector}').type('{uparrow}');`;
-    case 'ArrowRight':
-      return `cy.get('${event.selector}').type('{rightarrow}');`;
-    case 'ArrowDown':
-      return `cy.get('${event.selector}').type('{downarrow}');`;
-    case 'ArrowLeft':
-      return `cy.get('${event.selector}').type('{leftarrow}');`;
-    default:
-      return null;
-  }
+    const typeKey = {
+        Backspace: "{backspace}",
+        Escape: "{esc}",
+        ArrowUp: "{uparrow}",
+        ArrowRight: "{rightarrow}",
+        ArrowDown: "{downarrow}",
+        ArrowLeft: "{leftarrow}"
+    };
+
+    return event.key in typeKey
+        ? `cy.${selection(event)}.type(${typeKey[event.key]})`
+        : null;
 }
 
 function handleChange(event: ParsedEvent): string {
-  if (event.inputType === 'checkbox' || event.inputType === 'radio') return null;
-  return `cy.get('${event.selector}').type('${event.value.replace(/'/g, "\\'")}');`;
+    if (event.inputType === "checkbox" || event.inputType === "radio")
+        return null;
+    return `cy.${selection(event)}.type('${event.value.replace(
+        /'/g,
+        "\\'"
+    )}');`;
 }
 
 function handleDoubleclick(event: ParsedEvent): string {
-  return `cy.get('${event.selector}').dblclick();`;
+    return `cy.${selection(event)}.dblclick();`;
 }
 
 function handleSubmit(event: ParsedEvent): string {
-  return `cy.get('${event.selector}').submit();`;
+    return `cy.${selection(event)}.submit();`;
 }
 
 function handleUrl(url: string): string {
-  const { origin, pathname } = new URL(url);
-  return `cy.url().should('contains', '${origin + pathname}');`;
+    const { origin, pathname } = new URL(url);
+    return `cy.url().should('contains', '${origin + pathname}');`;
 }
 
 export default {
-  createBlock: (event: ParsedEvent): string => {
-    switch (event.action) {
-      case EventType.CLICK:
-        return handleClick(event);
-      case EventType.KEYDOWN:
-        return handleKeydown(event);
-      case EventType.CHANGE:
-        return handleChange(event);
-      case EventType.DBLCLICK:
-        return handleDoubleclick(event);
-      case EventType.SUBMIT:
-        return handleSubmit(event);
-      default:
-        throw new Error(`Unhandled event: ${event.action}`);
-    }
-  },
-  createVisit: (url: string): string => `cy.visit('${url}');`,
-  createUrl: (url: string): string => handleUrl(url),
+    createBlock: (event: ParsedEvent): string => {
+        switch (event.action) {
+            case EventType.CLICK:
+                return handleClick(event);
+            case EventType.KEYDOWN:
+                return handleKeydown(event);
+            case EventType.CHANGE:
+                return handleChange(event);
+            case EventType.DBLCLICK:
+                return handleDoubleclick(event);
+            case EventType.SUBMIT:
+                return handleSubmit(event);
+            default:
+                throw new Error(`Unhandled event: ${event.action}`);
+        }
+    },
+    createVisit: (url: string): string => `cy.visit('${url}');`,
+    createUrl: (url: string): string => handleUrl(url)
 };
